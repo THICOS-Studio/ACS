953ce7afe4fdbc92de1361083a2f6252263bae93
fangxm233 <1126716733@qq.com>
2017-07-23 20:25:14 +0800

    fangxm

--- Source/ACS_Analyzer/BNF_Engine/BNF.cs
+++ Source/ACS_Analyzer/BNF_Engine/BNF.cs
@@ -12,6 +12,7 @@ namespace ACS_Analyzer.BNF_Engine
         public static void Match(List<Token> _queue)
         {
             List<Token> input = new List<Token>();
+            //这里是把语句一句一句扔给匹配的
             foreach (Token item in _queue)
             {
                 if (item == null) break;
@@ -22,23 +23,66 @@ namespace ACS_Analyzer.BNF_Engine
                     input = new List<Token>();
                     continue;
                 }
+                if (item.GetValue() == "{")
+                {
+                    input.Add(item);
+                    MatchRules(input);
+                    input = new List<Token>();
+                    continue;
+                }
+                if (item.GetValue() == "}")
+                {
+                    input.Add(item);
+                    MatchRules(input);
+                    input = new List<Token>();
+                    continue;
+                }
                 input.Add(item);
             }
+            
         }
 
         static void MatchRules(List<Token> _input)
         {
             Parser parser = new Parser(_input);
-            //parser = parser.rule(Types.Identifier).rule("=").rule(Types.Number).rule(";");
-            parser = parser.rule(Types.Identifier).rule("=").rule(Types.Number).StartRegion().or("+").or("-").or("*").or("/").rule(Types.Number).EndRegion().rule(";");
-            /*
-            foreach (Token item in parser.new_queue)
-            {
-                Console.WriteLine(item.GetValue());
-            }*/
-            //if (parser.is_matched) Console.WriteLine("yeal");
 
-            //进行报错提示语法错误
+            #region 匹配
+            parser = parser.rule("if").rule("(").or(Types.Identifier).or(Types.Float).or(Types.Number).or(Types.String).rule("==")
+                .or(Types.Identifier).or(Types.Float).or(Types.Number).or(Types.String).rule(")").rule("{");
+            if (parser.is_matched) return;
+            parser = new Parser(_input);
+            parser = parser.rule("}");
+            if (parser.is_matched) return;
+            parser = new Parser(_input);
+            parser = parser.rule(Types.Identifier).rule("=").rule(Types.Number).LoopStart().or("+").or("-").or("*").or("/").rule(Types.Number).LoopEnd().rule(";");
+            if (parser.is_matched) return;
+            parser = new Parser(_input);
+            parser = parser.rule(Types.Identifier).rule("=").rule(Types.Number).LoopStart().or("+").or("-").or("*").or("/").rule(Types.Float).LoopEnd().rule(";");
+            if (parser.is_matched) return;
+            parser = new Parser(_input);
+            parser = parser.rule(Types.Identifier).rule("=").rule(Types.Identifier).LoopStart().or("+").or("-").or("*").or("/").rule(Types.Identifier).LoopEnd().rule(";");
+            if (parser.is_matched) return;
+            parser = new Parser(_input);
+            parser = parser.rule(Types.Identifier).rule("=").rule(Types.Number).rule(";");
+            if (parser.is_matched) return;
+            parser = new Parser(_input);
+            parser = parser.rule(Types.Identifier).rule("=").rule(Types.Float).rule(";");
+            if (parser.is_matched) return;
+            parser = new Parser(_input);
+            parser = parser.rule("print").rule("(").or(Types.Identifier).or(Types.Number).or(Types.String).or(Types.Float).rule(")").rule(";");
+            if (parser.is_matched) return;
+            parser = new Parser(_input);
+            parser = parser.rule("input").rule("(").rule(Types.Identifier).rule(")").rule(";");
+            if (parser.is_matched) return;
+            parser = new Parser(_input);
+            parser = parser.rule("[").rule(Types.Identifier).rule("]");
+            if (parser.is_matched) return;
+            parser = new Parser(_input);
+            parser = parser.rule("goto").rule(Types.Identifier).rule(";");
+            if (parser.is_matched) return;
+            #endregion
+
+            //在这里进行花式报错
         }
     }
 
@@ -47,6 +91,7 @@ namespace ACS_Analyzer.BNF_Engine
         public List<Token> queue;
         public List<Token> new_queue = new List<Token>();
         int now_count;
+        bool in_loop;
         bool in_region;
         List<string> command = new List<string>();
         List<string> parameter = new List<string>();
@@ -54,7 +99,7 @@ namespace ACS_Analyzer.BNF_Engine
         Parser parser;
         public bool is_matched = true; //表示整个语句是否匹配成功
         bool is_or_matched; //表示or语句是否匹配成功
-        bool in_for;
+        bool is_region_matched = true;
         
         public Parser(List<Token> _queue)
         {
@@ -69,29 +114,41 @@ namespace ACS_Analyzer.BNF_Engine
         }
         void AddCommand(string c, string s)
         {
-            if (in_region)
+            if (in_loop)
             {
                 command.Add(c);
                 parameter.Add(s);
             }
         }
-
-        public Parser StartRegion()
+        void AddCommand(string c)
         {
-            in_region = true;
+            if (in_loop)
+            {
+                command.Add(c);
+            }
+        }
+
+
+        public Parser LoopStart()
+        {
+            AddCommand("LoopStart");
+            in_loop = true;
             return this;
         }
         
         //这个函数会生成一个和现在这个数值一致的parser实例，并把先前记录的 匹配动作和参数 循环执行，直到没有新的匹配，循环结束，返回带有匹配结果的parser对象
-        public Parser EndRegion()
+        public Parser LoopEnd()
         {
+            AddCommand("LoopEnd");
             parser = new Parser(queue)
             {
                 now_count = now_count,
                 new_queue = new_queue,
                 is_matched = is_matched,
                 is_or_matched = is_or_matched,
-                in_for = true
+                in_loop = true,
+                in_region = in_region,
+                is_region_matched = is_region_matched
             };
             Parser _parser = new Parser(parser.queue)
             {
@@ -99,13 +156,15 @@ namespace ACS_Analyzer.BNF_Engine
                 new_queue = parser.new_queue,
                 is_matched = parser.is_matched,
                 is_or_matched = parser.is_or_matched,
-                in_for = true
+                in_loop = true,
+                in_region = in_region,
+                is_region_matched = is_region_matched
             };
             while (true)
             {
                 if (!parser.is_matched)
                 {
-                    _parser.in_for = false;
+                    _parser.in_loop = false;
                     return _parser;
                 }
                 _parser = new Parser(parser.queue)
@@ -114,7 +173,7 @@ namespace ACS_Analyzer.BNF_Engine
                     new_queue = parser.new_queue,
                     is_matched = parser.is_matched,
                     is_or_matched = parser.is_or_matched,
-                    in_for = true
+                    in_loop = true
                 };
                 for (int i = 0; i < command.Count; i++)
                 {
@@ -130,10 +189,49 @@ namespace ACS_Analyzer.BNF_Engine
                                 parser = parser.or(parameter[i]);
                                 break;
                             }
+                        case "LoopStart":
+                            {
+                                parser = parser.LoopStart();
+                                break;
+                            }
+                        case "LoopEnd":
+                            {
+                                parser = parser.LoopEnd();
+                                break;
+                            }
+                        case "RegionStart":
+                            {
+                                parser = parser.RegionStart();
+                                break;
+                            }
+                        case "RegionEnd":
+                            {
+                                parser = parser.RegionEnd();
+                                break;
+                            }
                     }
                 }
             }
         }
+        
+        public Parser RegionStart()
+        {
+            AddCommand("RegionStart");
+            in_region = true;
+            is_region_matched = true;
+            return this;
+        }
+
+        public Parser RegionEnd()
+        {
+            AddCommand("RegionEnd");
+            if (is_matched)
+                is_matched = is_region_matched;
+            in_region = false;
+            is_region_matched = true;
+            return this;
+        }
+        
         public Parser rule(string s)
         {
             is_or_matched = false;
@@ -150,11 +248,12 @@ namespace ACS_Analyzer.BNF_Engine
                 MatchSuccess();
                 return this;
             }
-            else if (queue[now_count].GetTokenType().ToString() == s && in_for)
+            else if (queue[now_count].GetTokenType().ToString() == s && in_loop)
             {
                 MatchSuccess();
                 return this;
             }
+            if (in_region) is_region_matched = false;
             is_matched = false;
             return this;
         }
@@ -174,6 +273,7 @@ namespace ACS_Analyzer.BNF_Engine
                 MatchSuccess();
                 return this;
             }
+            if (in_region) is_region_matched = false;
             is_matched = false;
             return this;
         }
@@ -181,6 +281,7 @@ namespace ACS_Analyzer.BNF_Engine
         {
             //Console.WriteLine(is_matched);
             AddCommand("or", s);
+            if (queue.Count == now_count) return this;
             if (is_or_matched) return this;
             is_matched = false;
             if (queue[now_count].GetValue() == ";")
@@ -194,12 +295,14 @@ namespace ACS_Analyzer.BNF_Engine
                 is_or_matched = true;
                 return this;
             }
+            if (in_region) is_region_matched = false;
             is_matched = false;
             return this;
         }
         public Parser or(Types t)
         {
             AddCommand("or", t.ToString());
+            if (queue.Count == now_count) return this;
             if (is_or_matched) return this;
             is_matched = false;
             if (queue[now_count].GetValue() == ";")
@@ -213,8 +316,11 @@ namespace ACS_Analyzer.BNF_Engine
                 is_or_matched = true;
                 return this;
             }
+            if (in_region) is_region_matched = false;
             is_matched = false;
             return this;
         }
     }
+
+
 }
--- Source/ACS_Analyzer/Program.cs
+++ Source/ACS_Analyzer/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ACS_Analyzer.BNF_Engine;
+using ACS_Lexer;
 
 namespace ACS_Analyzer
 {
@@ -14,9 +15,14 @@ namespace ACS_Analyzer
             System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
             watch.Start();  //开始监视代码运行时间
             //ACS_Parser.Parser p=new ACS_Parser.Parser(ACS_Lexer.Lexer._Main());
-            BNF.Match(ACS_Lexer.Lexer._Main());
-            watch.Stop();
+            List<Token> queue = Lexer._Main();
             TimeSpan timespan = watch.Elapsed;
+            Console.WriteLine("执行时间：{0}(毫秒)", timespan.TotalMilliseconds);
+            watch = new System.Diagnostics.Stopwatch();
+            watch.Start();  //开始监视代码运行时间
+            BNF.Match(queue);
+            watch.Stop();
+            timespan = watch.Elapsed;
             Console.WriteLine("执行时间：{0}(毫秒)", timespan.TotalMilliseconds);
             Console.ReadKey();
 
